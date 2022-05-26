05896dc7797aa35ed8467c70bb642e9619dcb487
Toritae <ilieys6482@gmail.com>
2022-05-26 16:01:37 +0900

    Updates

---
+++ precache-manifest.b70e3300ef787ad6541a1243d88d2165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6503c2706c2321858bc8bbe8e3cc7ead",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9f2a2eb80c5520cae4cc",
+    "url": "/portfolio/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "f62ed34f650d4c447265",
+    "url": "/portfolio/static/css/main.092d739f.chunk.css"
+  },
+  {
+    "revision": "9f2a2eb80c5520cae4cc",
+    "url": "/portfolio/static/js/2.498b28c1.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.498b28c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62ed34f650d4c447265",
+    "url": "/portfolio/static/js/main.3200ed4c.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
